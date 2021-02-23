052030f6064adb4da61b12c4875bc6576cf620aa
Lawrence Tray <ltray99@gmail.com>
2021-02-23 18:24:08 +0000

    Simple program

---
+++ simple.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+
+void thread_init()
+{
+    std::cout << "This statement is executed in the new thread\n";
+}
+
+int main()
+{
+    std::thread t(thread_init);
+    std::cout << "This statement is executed in the main thread\n";
+    t.join();
+}
